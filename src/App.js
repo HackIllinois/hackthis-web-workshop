840aa176c1264da2666464d098ac7e2cd6ee744c
Rem Yang <remyang2@illinois.edu>
2020-08-07 20:18:14 -0500

    Push starter code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
 import React, { Component } from 'react';
-import CountrySelect from './components/CountrySelect';
-import CountryInfo from './components/CountryInfo';
 
 import './App.css';
 
-class App extends Component {
-    state = {
-        selectedCountry: null
-    }
-
-    changeCountry = (e) => {
-        this.setState(
-            {selectedCountry: e.target.value}
-        );
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <h1>COVID-19 Data by Country</h1>
-                <CountrySelect changeCountry={this.changeCountry} />
-                <CountryInfo selectedCountry={this.state.selectedCountry} />
-            </div>
-        );
-    }
+export default class App extends Component {
+    
 }
-
-export default App;
